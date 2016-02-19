f8d6877f08c845dde75ca7b77be3af29e9215b39
SlavaPihidko <slava17puh@gmail.com>
2016-02-19 12:49:26 +0200

    изменена строчка вывода на екран в программе Hello

--- lesson_1/src/main/java/qa/paket1/Hello.java
+++ lesson_1/src/main/java/qa/paket1/Hello.java
@@ -2,6 +2,6 @@ package qa.paket1;
 
 public class Hello {
     public static void main(String[] args) {
-          System.out.println("qa.paket1.Hello world!");
+      System.out.println("Hello world!");
 	}
 	}
