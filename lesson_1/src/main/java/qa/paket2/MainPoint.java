3c67779eedb5d391be7cf04271c00979c5c1c2b2
SlavaPihidko <slava17puh@gmail.com>
2016-02-20 01:02:32 +0200

    рабочая программа
    реализован метод p.distance(p1, p2);
    значения точек устанавливаютсья через конструктор.

--- lesson_1/src/main/java/qa/paket2/MainPoint.java
+++ lesson_1/src/main/java/qa/paket2/MainPoint.java
@@ -5,18 +5,13 @@ package qa.paket2;
  */
 public class MainPoint {
   public static void main(String[] args) {
-    Point p1 = new Point();
-    {
-      p1.x = 17;
-      p1.y = 15;
-    }
-    Point p2 = new Point();
-    {
-      p2.x = 37;
-      p2.y = 45;
-    }
+
+    Point p1 = new Point(17, 15);
+
+    Point p2 = new Point(37, 45);
+
     Point p = new Point();
-    p.distance(p1,p2);
+      p.distance(p1, p2);
   }
 }
 
--- lesson_1/src/main/java/qa/paket2/Point.java
+++ lesson_1/src/main/java/qa/paket2/Point.java
@@ -4,18 +4,21 @@ package qa.paket2;
  * Created by Slava on 19.02.2016.
  */
 public class Point {
-  double x;
-  double y;
+  private double x;
+  private double y;
 
-  /* public static void setdistance(Point p1,Point p2){
+  Point(){}
 
-  }*/
+  Point(double x, double y){
+    this.x=x;
+    this.y=y;
+  }
 
-  public static double distance(Point p1,Point p2){
-    double rezx=p1.x-p2.x;
-    double rezy=p1.y-p2.y;
-    double z=Math.sqrt(rezx*rezx+rezy*rezy);
-    System.out.println("The distance between 2 points equal = " + z);
+  public double distance(Point p1,Point p2){
+    double rezx = p1.x-p2.x;
+    double rezy = p1.y-p2.y;
+    double z = Math.sqrt(rezx*rezx+rezy*rezy);
+    System.out.println("The distance between 2 points equals = " + z);
     return z;
   }
 }
