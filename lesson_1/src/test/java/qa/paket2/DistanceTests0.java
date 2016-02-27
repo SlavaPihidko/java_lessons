64ef7470ad9e39752504466af957dd6e8bfc07c8
SlavaPihidko <slava17puh@gmail.com>
2016-02-27 18:51:46 +0200

    два теста в одном файле

--- lesson_1/src/test/java/qa/paket2/DistanceTests0.java
+++ lesson_1/src/test/java/qa/paket2/DistanceTests0.java
@@ -9,11 +9,19 @@ import org.testng.annotations.Test;
 public class DistanceTests0 {
 
   @Test
-  public void testDistance(){
-    Point p3 = new Point(17,15);
-    Point p4 = new Point(37,45);
+  public void testDistance() {
+    Point p3 = new Point(17, 15);
+    Point p4 = new Point(37, 45);
     Point p5 = new Point();
-    Assert.assertEquals(p5.distance(p3,p4),36.0);
+    Assert.assertEquals(p5.distance(p3, p4), 36.0);
+    //assert p5.distance(p3,p4)==36.0;
+  }
 
+  @Test
+  public void testDistance7() {
+    Point p3 = new Point(0, 0);
+    Point p4 = new Point(0, 0);
+    Point p5 = new Point();
+    Assert.assertEquals(p5.distance(p3, p4), 0.0);
   }
 }
