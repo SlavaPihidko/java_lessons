226be4f8a58704d647719ecf7723c9da4c1c28e2
SlavaPihidko <slava17puh@gmail.com>
2016-03-01 13:59:07 +0200

    создан клас TestBase
    повторяющиеся методы с class GroupCreationTests перенесены в
    родительский класс

---
+++ addressbook-web-tests/src/test/java/qa/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package qa.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    
+    @Test
+    public void testsGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
