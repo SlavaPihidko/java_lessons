9bebf97826b3584156462ea39eb3a29577fe0147
SlavaPihidko <slava17puh@gmail.com>
2016-03-03 19:19:30 +0200

    добвлена возможность выбирать браузер

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package qa.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import qa.addressbook.appmanager.ApplicationManager;
@@ -9,7 +10,7 @@ import qa.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
