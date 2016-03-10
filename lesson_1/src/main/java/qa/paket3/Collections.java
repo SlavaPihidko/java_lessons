3bde6c1d8e988a117eaae2ff71adcba7b48a672c
SlavaPihidko <slava17puh@gmail.com>
2016-03-10 19:36:51 +0200

    урок "по спискам"

---
+++ lesson_1/src/main/java/qa/paket3/Collections.java
@@ -0,0 +1,32 @@
+package qa.paket3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Slava on 10.03.2016.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"Java","C#","Python","PHP"};
+
+    List<String> languages = Arrays.asList("Java","C#","Python","PHP");
+    /* List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    languages.add("PHP"); */
+
+   for(String l: languages){
+      System.out.println("Я хочу выучить "+l);
+    }
+
+    /* for(int i=0; i<languages.size();i++) {
+      System.out.println("Я хочу выучить "+languages.get(i));
+    } */
+
+  }
+
+}
