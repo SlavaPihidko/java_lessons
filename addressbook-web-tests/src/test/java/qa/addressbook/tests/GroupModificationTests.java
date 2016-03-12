cbcb08b48f97397ba796e716c92f8d29038872c4
SlavaPihidko <slava17puh@gmail.com>
2016-03-12 18:02:47 +0200

    Проверка(сравнение) групп после модификации
    Добавления поля ID чтобы отличать группы. Проверка: Сравнение
    групп(множеств) после модификации.

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import qa.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -19,15 +20,18 @@ public class GroupModificationTests extends TestBase {
     }
     //int before = app.getGroupHelper().getGroupCount();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(0);
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test_group",
-                                                      "test pole 1",
-                                                      "test pole 3"));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(), "test1", "test pole 1", "test pole 3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModefication();
     app.getGroupHelper().returnToGroupPage();
    // int after = app.getGroupHelper().getGroupCount();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size());
+
+    before.remove(before.size()-1);
+    before.add(group);
+    Assert.assertEquals( new HashSet<Object>(before), new HashSet<Object>(after) );
   }
 }
