8e766f6a2cf28ffc26b9332cec626d04ec3840d5
SlavaPihidko <slava17puh@gmail.com>
2016-03-13 14:42:11 +0200

    Добавлена проверка множеств при создании новой группы
    для проверки мы находим макс ID новой группы со списка существующих
    групп

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import qa.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,10 +14,21 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     //int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().creationGroups(new GroupData("test_group", null, null));
+    GroupData group = new GroupData("test_group", null, null);
+    app.getGroupHelper().creationGroups(group);
     //int after = app.getGroupHelper().getGroupCount();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()+1);
+
+    int max=0;
+    for(GroupData g: after){
+      if(g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals( new HashSet<Object>(before), new HashSet<Object>(after) );
   }
 
 }
