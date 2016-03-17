47be3efe85d695cd60e5108ead3f9a0abb9634be
SlavaPihidko <slava17puh@gmail.com>
2016-03-17 13:59:50 +0200

    запуск тестов набором Suite

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/ContactCreationTests.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class ContactCreationTests extends TestBase {
 
 
-  @Test
+  @Test(enabled=false)
   public void testsAddNewAddress() {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
