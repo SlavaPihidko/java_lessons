176be86387cf496dd80b1d2ab062a0d8bd5ad932
SlavaPihidko <slava17puh@gmail.com>
2016-03-27 18:47:39 +0300

    Реализация(новая) проверки адреса

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/ContactAddressTests.java
@@ -3,6 +3,9 @@ package qa.addressbook.tests;
 import org.testng.annotations.Test;
 import qa.addressbook.model.ContactData;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -17,6 +20,18 @@ public class ContactAddressTests extends TestBase {
     ContactData contact = app.getContactHelper().all().iterator().next();
     ContactData addressInfoFromEditForm = app.getContactHelper().infoFromEditForm(contact);
 
-    assertThat(contact.getAddress(), equalTo(addressInfoFromEditForm.getAddress()));
+    assertThat(contact.getAddress(), equalTo(mergeAddress(addressInfoFromEditForm)));
   }
+
+  private String mergeAddress(ContactData contact) {
+    return Arrays.asList(contact.getAddress())
+            .stream()
+            .map(ContactAddressTests:: cleaned)
+            .collect(Collectors.joining(" "));
+  }
+
+  public static String cleaned(String address) {
+    return address.replaceAll("\\s", "");
+  }
+
 }
