0623f22831ac10d1d687d5df293db4e989c8961c
SlavaPihidko <slava17puh@gmail.com>
2016-03-30 01:00:06 +0300

    Исправлены ошибки при которых падале тесты для проверки телеф, и адресов

--- addressbook-web-tests/src/test/java/qa/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/appmanager/ContactHelper.java
@@ -152,7 +152,7 @@ public class ContactHelper extends HelperBase {
   }
 
   public ContactData infoFromEditForm(ContactData contact) {
-    modifyInAllDataOfContact();
+    initContactModification(contact.getId());
     String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
     String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
     String address = wd.findElement(By.name("address")).getAttribute("value");
@@ -171,6 +171,24 @@ public class ContactHelper extends HelperBase {
 
   public ContactData infoFromEditForm() {
     modifyInAllDataOfContact();
+    String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+    String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+    String address = wd.findElement(By.name("address")).getAttribute("value");
+    String email = wd.findElement(By.name("email")).getAttribute("value");
+    String email2 = wd.findElement(By.name("email2")).getAttribute("value");
+    String email3 = wd.findElement(By.name("email3")).getAttribute("value");
+    String home = wd.findElement(By.name("home")).getAttribute("value");
+    String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+    String work = wd.findElement(By.name("work")).getAttribute("value");
+    wd.navigate().back(); // навигация назад
+    return new ContactData().withFirstName(firstname).withLastName(lastname)
+            .withAddress(address).withEmail(email).withEmail2(email2).withEmail3(email3)
+            .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+
+  }
+
+  public ContactData infoFromEditFormForPhoneAddressEmail(ContactData contact) {
+    initContactModification(contact.getId());
     String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
     String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
     String address = wd.findElement(By.name("address")).getAttribute("value");
--- addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
@@ -12,7 +12,7 @@ import qa.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeSuite
   public void setUp() throws Exception {
