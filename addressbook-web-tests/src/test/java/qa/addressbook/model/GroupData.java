cabf72f6458ff89185bcc4f092bc3682945c4c9f
SlavaPihidko <slava17puh@gmail.com>
2016-03-31 00:26:20 +0300

    Создание параметризованых тестовых методов

--- addressbook-web-tests/src/test/java/qa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/model/GroupData.java
@@ -63,8 +63,10 @@ public class GroupData {
   @Override
   public String toString() {
     return "GroupData{" +
-            "name='" + name + '\'' +
-            ", id='" + id + '\'' +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", header='" + header + '\'' +
+            ", footer='" + footer + '\'' +
             '}';
   }
 
--- addressbook-web-tests/src/test/java/qa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/GroupCreationTests.java
@@ -1,22 +1,35 @@
 package qa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import qa.addressbook.model.GroupData;
 import qa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
 
-    @Test
-    public void testGroupCreation () {
-      for(int i = 1;i>=1;i--) {
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation (GroupData group) {
+      //for(int i = 1;i>=1;i--) {
 
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test_group");
     app.group().create(group);
     assertThat(app.group().getGroupCount(), equalTo(before.size() + 1));
     Groups after = app.group().all();
@@ -30,8 +43,7 @@ public class GroupCreationTests extends TestBase {
     group.withId(max);
 
     assertThat(after, equalTo(before.withAdded(group)));
-  }
-
+ // }
   }
 
  /* @Test
