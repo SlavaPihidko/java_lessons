cc9b847835d24e598d71343676a423e979004471
SlavaPihidko <slava17puh@gmail.com>
2016-04-01 23:42:56 +0300

    Реализована загрузка информации о тестовом стенде  из конфигурационного файла

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
@@ -12,7 +12,9 @@ import qa.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  //protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX)); //взять значение системного свойства "browser"
 
   @BeforeSuite
   public void setUp() throws Exception {
