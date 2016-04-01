c95d480bf48b8601a3292da69bcb89635b14751e
SlavaPihidko <slava17puh@gmail.com>
2016-04-01 18:47:00 +0300

    Исрользование конструкции try автомат. закрытия файлов записи\чтения

--- addressbook-web-tests/src/test/java/qa/addressbook/Generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/Generators/GroupDataGenerator.java
@@ -67,9 +67,9 @@ public class GroupDataGenerator {
     //xstream.alias("group", GroupData.class);
     xstream.processAnnotations(GroupData.class);
     String xml = xstream.toXML(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try(Writer writer = new FileWriter(file)){ //второе назначение использования блока try для автоматического закрытия файлов
+      writer.write(xml);
+    }
   }
 
   private  void saveAsCsv(List<GroupData> groups, File file) throws IOException {
