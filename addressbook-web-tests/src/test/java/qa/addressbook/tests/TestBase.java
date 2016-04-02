a58dc9402851a372dcef51854842e58a62143d0f
SlavaPihidko <slava17puh@gmail.com>
2016-04-02 17:10:16 +0300

    Добавление библиотеки LOGBack и Реализация логгирования тестов

--- addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/addressbook/tests/TestBase.java
@@ -1,16 +1,23 @@
 package qa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import qa.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 /**
  * Created by Slava on 01.03.2016.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   //protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
   protected static final ApplicationManager app
@@ -21,9 +28,19 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p){
+    logger.info("Start test " + m.getName() + "with parameters "+ Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("Stop test " + m.getName());
   }
 
 }
